f2ef24e3216040348124f40b40563f5bc448646b
EduhG <edwinnyangena@gmail.com>
2016-07-16 21:44:02 +0300

    Updated app.js file

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 var fireBaseRef = new Firebase("https://chatapp-ba088.firebaseio.com/");
+
+$("#post-button").bind("click", function() {
+    var comment = $("#message");
+    var comment_value = $.trim(comment.val());
+    
+    if (commentValue.length === 0) {
+        alert('Messages can not be blank!');
+    } else {
+        _fireBaseRef.push({comment: commentValue}, function(error) {
+            if (error !== null) {
+                alert('Unable to post your message!. Try again later.');
+            }
+        });
+ 
+        comment.val("");
+    }
+    return false;
+});
